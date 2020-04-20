feaca1e109bab7cca0cf25116045c6aa27b7aacc
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-20 04:16:07 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -1,15 +1,19 @@
-class CreateArtists < ActiveRecord::Migration[5.2]
-  def change
-  end 
-  
+class CreateArtists < ActiveRecord::Migration
+  def up
+  end
+
+  def down
+  end
+
   def change
     create_table :artists do |t|
       t.string :name
       t.string :genre
       t.integer :age
       t.string :hometown
-    end
-    
-end 
+    end 
+  end
+
+end
 
 #This lesson was originally created with gem versions that support Rails 5.2, so we need to make have our CreateArtist migration inherit from ActiveRecord::Migration[5.2].
