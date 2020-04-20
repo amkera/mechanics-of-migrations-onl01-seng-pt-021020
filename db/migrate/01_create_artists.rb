06c2a50d84a007df4907a9bfc6cc4eaaf9937c3f
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-20 04:18:07 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -16,4 +16,6 @@ class CreateArtists < ActiveRecord::Migration
 
 end
 
+#On the left we've given the data type we'd like to cast the column as, and on the right, we've given the name we'd like to give the column. The only thing that we're missing is the primary key. Active Record will generate that column for us, and for each row added, a key will be auto-incremented.
+
 #This lesson was originally created with gem versions that support Rails 5.2, so we need to make have our CreateArtist migration inherit from ActiveRecord::Migration[5.2].
