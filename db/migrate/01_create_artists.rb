a87899d77ab82552e915faea866f322e6398fc4e
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-20 04:14:00 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -6,6 +6,15 @@ class CreateArtists < ActiveRecord::Migration[5.2]
   def down 
     #defines the code to execute when the migration is rolled back. "undo"
   end 
+  
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+    
 end 
 
 #This lesson was originally created with gem versions that support Rails 5.2, so we need to make have our CreateArtist migration inherit from ActiveRecord::Migration[5.2].
